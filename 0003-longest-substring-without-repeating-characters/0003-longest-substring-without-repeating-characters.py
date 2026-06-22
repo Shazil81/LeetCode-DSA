006052f8ab60abef00ed782f3caae56a8174c962
Shazil81 <ahmadshahab890@gmail.com>
2026-06-23 01:21:05 +0530

    Time: 4 ms (98.96%), Space: 19.5 MB (7.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Brute force
+        # Optimal solution by using two pointers
+        my_dict = {} # dict use kiya
+        left = 0
+        right = 0
         maxi = 0
         n = len(s)
-        for i in range(n):
-            my_set = set()
-            for j in range(i, n):
-                if s[j] in my_set:
-                    break
-                maxi = max(maxi, j-i+1) # sliding window me ek technique hai j-i+1 ek window ka length hota h waise
-                my_set.add(s[j])
+        while right < n:
+            if s[right] in my_dict:
+                # agar mera dict me hoga to hm kya krenge ki left ko jha pe mila h us index se ek aage badha denge
+                # ek edge case kya hai max lene ka ki ho skta h ki pehle se ho dict me lekin left wha pe nhi ho to kya hoga ki window to valid hoga is liye max lena h left ka or jha pe aaya hai wha pe se ka aage ka index ka 
+                left = max(left, my_dict[s[right]]+1)
+            # two pointers me right-left+1 ek window length ho jata h  
+            maxi = max(maxi, right-left+1)
+            my_dict[s[right]] = right # dict me index ko store kr dena
+            right+=1
         return maxi
 
