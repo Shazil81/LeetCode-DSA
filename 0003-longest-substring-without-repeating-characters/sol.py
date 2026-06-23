23d737964a0fe89bc5db9cf5d266e717c29a2e18
Shazil81 <ahmadshahab890@gmail.com>
2026-06-23 11:43:06 +0530

    Add solution for longest substring without repeating characters
    Implement brute force method to find the length of the longest substring without repeating characters.

---
+++ 0003-longest-substring-without-repeating-characters/sol.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Brute force
+        maxi = 0
+        n = len(s)
+        for i in range(n):
+            my_set = set()
+            for j in range(i, n):
+                if s[j] in my_set:
+                    break
+                maxi = max(maxi, j-i+1) # sliding window me ek technique hai j-i+1 ek window ka length hota h waise
+                my_set.add(s[j])
+        return maxi
