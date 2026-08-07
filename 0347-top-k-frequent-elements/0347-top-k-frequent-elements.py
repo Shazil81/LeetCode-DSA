08843ea757cb3ead646a1144d964f9a351af754f
Shazil81 <ahmadshahab890@gmail.com>
2026-08-07 15:53:26 +0530

    Time: 3 ms (89.8%), Space: 22.7 MB (94.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # bucket sort : Optimal One
+        # Hashmap + sorting : Not Optimal
+        res = []
         n = len(nums)
-        bucket = [[] for i in range(n+1)] # n+1 banane k pichhe ek reason hai ki mera frequesncy 1 baar to aayega hi isi liye 0th index ko hmlog ignore kr denge
-        
+
         freq = {}
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
-        # Bucket me hashmap me jo value hoga yaani ki count usko index pe daal rhe h
-        for key, value in freq.items():
-            bucket[value].append(key)
         
-        res = []
-        for i in range(len(bucket)-1, 0, -1): # last se chala rhe h is liye kyun ki isme jo sab se zyada frequesncy hoga uska index last me hoga baaki sab khali rhega 
-            for num in bucket[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        sorted_items = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+        
+        # Step 3: Top K elements ko result list me daalna
+        result = []
+        for i in range(k):
+            result.append(sorted_items[i][0]) # sorted_items[i][0] se number milega
+            
+        return result
